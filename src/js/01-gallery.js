3c9847ffa8aaa2669faa8fb92d94e93f3f8dad80
Khrystyna Harasym <109437395+Khrystyna-Harasym@users.noreply.github.com>
2022-12-18 19:48:38 -0500

    completed task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
 console.log(galleryItems);
+console.log(createGalleryImages(galleryItems));
+
+const gallery = document.querySelector('.gallery');
+const imagesMarkup = createGalleryImages(galleryItems);
+gallery.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createGalleryImages(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+        `;
+    })
+    .join('');
+}
+gallery.addEventListener('click', onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  if (!evt.target === 'IMG') {
+    return;
+  }
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
