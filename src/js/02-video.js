c91d279d13aebe41b2be8e56e7f9805ca5beb7ff
Khrystyna Harasym <109437395+Khrystyna-Harasym@users.noreply.github.com>
2022-12-18 20:10:42 -0500

    completed task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const throttle = require('lodash.throttle');
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const onPlay = function (event) {
+  // data is an object containing properties specific to that event
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+};
+
+
+const time = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(time)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
